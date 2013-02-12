39a4caf544a568832466396cacaa5309111cc83a
Tim Dettrick <t.dettrick@uq.edu.au>
2013-02-12 11:02:51 +1000

    Initial code from template.

---
+++ README.md
@@ -0,0 +1,12 @@
+# AORRA Graph Demo #
+
+## Build & Run ##
+
+```sh
+$ cd AORRA_Graph_Demo
+$ ./sbt
+> container:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/test/scala/au/edu/uq/aorragraphdemo/AorraGraphDemoSpec.scala
@@ -0,0 +1,16 @@
+package au.edu.uq.aorragraphdemo
+
+import org.scalatra.test.specs2._
+
+// For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
+class AorraGraphDemoSpec extends ScalatraSpec { def is =
+  "GET / on AorraGraphDemo"                     ^
+    "should return status 200"                  ! root200^
+                                                end
+
+  addServlet(classOf[AorraGraphDemo], "/*")
+
+  def root200 = get("/") {
+    status must_== 200
+  }
+}
