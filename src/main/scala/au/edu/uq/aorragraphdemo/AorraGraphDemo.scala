c4a0fb1b8fe93d3d0b0d018a2b4e9bd9b795b01a
Tim Dettrick <t.dettrick@uq.edu.au>
2013-02-13 12:52:02 +1000

    Adding initial demo of progress bar.
    Still working out dimensions.

--- src/main/scala/au/edu/uq/aorragraphdemo/AorraGraphDemo.scala
+++ src/main/scala/au/edu/uq/aorragraphdemo/AorraGraphDemo.scala
@@ -2,13 +2,57 @@ package au.edu.uq.aorragraphdemo
 
 import org.scalatra._
 import scalate.ScalateSupport
-import java.io.File
-import javax.servlet.http.HttpServletRequest
+
+import java.awt.Dimension
+import java.io.ByteArrayOutputStream
+
+import ereefs.charts._
+import ereefs.images._
 
 class AorraGraphDemo extends ScalatraFilter with ScalateSupport {
 
   get("/") {
-    <html><head></head><body><p>Placeholder</p></body></html>
+    <html>
+	  <head>
+	  	<title>AORRA Graph Demo</title>
+	  </head>
+	  <body>
+	  	<ul>
+	  	  <li><a href="progress-chart?value=42">Progress Bar</a></li>
+	  	</ul>
+	  </body>
+	</html>
   }
+
+  get("/progress-chart") {
+    val builder = new ProgressChartBuilder(
+        multiParams("tl").headOption getOrElse "",
+        multiParams("tr").headOption getOrElse "",
+        multiParams("max").headOption map { _.toInt} getOrElse 100)
+    val chart = builder.get(multiParams("value").headOption match {
+      case Some(v) => v.toFloat
+      case None => halt(400, """Parameter "value" is required.""")
+    })
+
+    val dimension = (multiParams("width"), multiParams("height")) match {
+      case (Seq(x, _*), Seq(y, _*)) => new Dimension(x.toInt, y.toInt)
+      case _ => chart.getMinDimension
+    }
+    chart.setDimension(dimension)
+
+    val content = new ImageContent(new ImageRenderer {
+      def render = (new ChartRenderer(chart)).render
+    })
+
+    contentType = content.getContentType
+    transformToBytes(content)
+  }
+
+  private def transformToBytes(content: ImageContent) = {
+  	val byteStream = new ByteArrayOutputStream()
+    content.write(byteStream)
+    byteStream.toByteArray
+  }
+
 
 }
