fa210ff54e7e845e84272c2cc1489a40247752f6
Tim Dettrick <t.dettrick@uq.edu.au>
2013-02-13 13:47:38 +1000

    Adding proper scaling for progress bar.

--- src/main/scala/au/edu/uq/aorragraphdemo/AorraGraphDemo.scala
+++ src/main/scala/au/edu/uq/aorragraphdemo/AorraGraphDemo.scala
@@ -33,19 +33,27 @@ class AorraGraphDemo extends ScalatraFilter with ScalateSupport {
       case Some(v) => v.toFloat
       case None => halt(400, """Parameter "value" is required.""")
     })
+    chart.setDimension(chart.getMinDimension)
 
-    val dimension = (multiParams("width"), multiParams("height")) match {
-      case (Seq(x, _*), Seq(y, _*)) => new Dimension(x.toInt, y.toInt)
-      case _ => chart.getMinDimension
-    }
-    chart.setDimension(dimension)
+    val renderer = new ChartRenderer(chart)
+    renderer.setScale(rendererScale(chart.getDimension, multiParams))
 
-    val content = new ImageContent(new ImageRenderer {
-      def render = (new ChartRenderer(chart)).render
-    })
-
+    val content = chartImageContent(renderer)
     contentType = content.getContentType
     transformToBytes(content)
+  }
+
+  private def chartImageContent(renderer: ChartRenderer) = {
+    new ImageContent(new ImageRenderer {
+      def render = renderer.render
+    })
+  }
+
+  private def rendererScale(d: Dimension, multiParams: MultiParams) = {
+    def paramOption(k: String) = multiParams(k).headOption map { _.toInt }
+    new Dimension(
+        paramOption("width") getOrElse d.getWidth.toInt,
+        paramOption("height") getOrElse d.getHeight.toInt)
   }
 
   private def transformToBytes(content: ImageContent) = {
