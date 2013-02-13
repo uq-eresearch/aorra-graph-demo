8b284ad4ed9afd23adf00953ce462cf1dc9075bb
Tim Dettrick <t.dettrick@uq.edu.au>
2013-02-13 16:45:50 +1000

    Adding demo usage into index page.

--- src/main/scala/au/edu/uq/aorragraphdemo/AorraGraphDemo.scala
+++ src/main/scala/au/edu/uq/aorragraphdemo/AorraGraphDemo.scala
@@ -13,29 +13,62 @@ import ereefs.images._
 class AorraGraphDemo extends ScalatraFilter with ScalateSupport {
 
   get("/") {
-    var progressChartUrl = relativeUrl("progress-chart", Map(
-      "tl" -> "Horticulture",
-      "tr" -> "2013 targets",
-      "value" -> "42"
-    ))
     <html>
-	  <head>
-	  	<title>AORRA Graph Demo</title>
-	  </head>
-	  <body>
-    	<h1>Chart Examples</h1>
-	  	<ul>
-	  	  <li><a href={progressChartUrl}>Progress Chart</a></li>
-	  	  <li><a href="sugarcane-practice-chart">Sugar Practice Chart</a></li>
-	  	</ul>
-	  </body>
-	</html>
+      <head>
+        <title>AORRA Graph Demo</title>
+        <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <style type="text/css">
+        <![CDATA[
+        form label {
+            display: inline-block;
+            width: 6em;
+        }
+        ]]>
+        </style>
+      </head>
+      <body>
+        <h1>Chart Examples</h1>
+        <form id="progress-chart">
+          <fieldset>
+            <legend>Progress Chart</legend>
+            <label for="tl">Top Left Text</label>
+            <input type="text" name="tl" value="Horticulture"/><br/>
+            <label for="tr">Top Right Text</label>
+            <input type="text" name="tr" value="2013 targets"/><br/>
+            <label for="value">Value</label>
+            <input type="text" name="value" value="42"/><br/>
+            <label for="max">Max</label>
+            <input type="text" name="max" value="100"/><br/>
+            <label for="width">Dimensions</label>
+            <input type="text" name="width" size="3"/> &times;
+            <input type="text" name="height" size="3"/><br/>
+            <button type="submit">Render</button>
+            <hr />
+            <img/>
+          </fieldset>
+        </form>
+        <fieldset>
+          <legend>Sugar Practice Chart</legend>
+          <img src="sugarcane-practice-chart"/>
+        </fieldset>
+        <script type="text/javascript">
+        <![CDATA[
+        $('#progress-chart').submit(function() {
+          var serialize = $(this).serialize();
+          $(this).find('img').attr('src', 'progress-chart?'+serialize);
+          return false;
+        });
+        $('#progress-chart').submit();
+        ]]>
+        </script>
+      </body>
+    </html>
   }
 
   get("/sugarcane-practice-chart") {
     val dsBuilder = new CategoryDatasetBuilder()
     Seq("A","B","C","D") foreach { l =>
-	  Seq("08","09") foreach { n =>
+      Seq("08","09") foreach { n =>
         Seq("Nutrients", "Herbicides", "Soil") foreach { category =>
           val series = s"${l}_${n}"
           dsBuilder.addValue(25.0, series, category)
@@ -79,14 +112,20 @@ class AorraGraphDemo extends ScalatraFilter with ScalateSupport {
   }
 
   private def rendererScale(d: Dimension, multiParams: MultiParams) = {
-    def paramOption(k: String) = multiParams(k).headOption map { _.toInt }
+    def paramOption(k: String) = {
+      try {
+        multiParams(k).headOption map { _.toInt }
+      } catch {
+        case e: NumberFormatException => None
+      }
+    }
     new Dimension(
         paramOption("width") getOrElse d.getWidth.toInt,
         paramOption("height") getOrElse d.getHeight.toInt)
   }
 
   private def transformToBytes(content: Content) = {
-  	val byteStream = new ByteArrayOutputStream()
+      val byteStream = new ByteArrayOutputStream()
     content.write(byteStream)
     byteStream.toByteArray
   }
