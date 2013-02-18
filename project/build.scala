534b190ce4272065b2ada31217b9667d06d6fa4a
Tim Dettrick <t.dettrick@uq.edu.au>
2013-02-18 14:49:35 +1000

    Making usable on Heroku.
    We'll probably host elsewhere later, but this gets us up and running.

--- project/build.scala
+++ project/build.scala
@@ -4,6 +4,7 @@ import org.scalatra.sbt._
 import org.scalatra.sbt.PluginKeys._
 import com.mojolly.scalate.ScalatePlugin._
 import ScalateKeys._
+import com.typesafe.sbt.SbtStartScript
 
 object AorraGraphDemoBuild extends Build {
   val Organization = "au.edu.uq"
@@ -15,7 +16,7 @@ object AorraGraphDemoBuild extends Build {
   lazy val project = Project (
     "aorra-graph-demo",
     file("."),
-    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
+    settings = Defaults.defaultSettings ++ seq(SbtStartScript.startScriptForClassesSettings: _*) ++ ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
       organization := Organization,
       name := Name,
       version := Version,
@@ -27,8 +28,8 @@ object AorraGraphDemoBuild extends Build {
         "org.scalatra" %% "scalatra-scalatest" % "2.2.0" % "test",
         "org.scalamock" %% "scalamock-scalatest-support" % "3.0.1" % "test",
         "ch.qos.logback" % "logback-classic" % "1.0.6" % "runtime",
-        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
+        "org.eclipse.jetty" % "jetty-webapp" % "8.1.8.v20121106" % "compile;container",
+"org.eclipse.jetty.orbit" % "javax.servlet" % "3.0.0.v201112011016" % "compile;container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar")),
         "commons-io" % "commons-io" % "2.3",
         "org.jfree" % "jfreechart" % "1.0.14",
         "org.apache.commons" % "commons-lang3" % "3.1",
@@ -43,7 +44,9 @@ object AorraGraphDemoBuild extends Build {
             Some("templates")
           )
         )
-      }
+      },
+      mainClass in Compile := Some("JettyLauncher")
     )
   )
+
 }
