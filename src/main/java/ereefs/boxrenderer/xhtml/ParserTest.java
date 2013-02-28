d8aa8220d9fb6dda9aedb401950109052a1bd5f4
Tim Dettrick <t.dettrick@uq.edu.au>
2013-02-28 15:36:59 +1000

    Moving ParserTest, as it's not strictly a test in the Maven/SBT sense.
    It can be run in Eclipse, or with:
    ./sbt "run-main ereefs.boxrenderer.xhtml.ParserTest"

--- src/main/java/ereefs/boxrenderer/xhtml/ParserTest.java
+++ src/main/java/ereefs/boxrenderer/xhtml/ParserTest.java
@@ -16,22 +16,22 @@ import ereefs.boxrenderer.xhtml.Parser.Resolver;
 
 public class ParserTest {
 
-	private static final String BURDEKIN_STATUS = "/ereefs/boxrenderer/xhtml/status_burdekin.xml";
-	private static final String CAPE_YORK_STATUS ="/ereefs/boxrenderer/xhtml/status_capeyork.xml";
+    private static final String BURDEKIN_STATUS = "status_burdekin.xml";
+    private static final String CAPE_YORK_STATUS ="status_capeyork.xml";
 
-	private static InputStream getStream(String resource) throws Exception  {
+    private static InputStream getStream(String resource) throws Exception  {
         InputStream in = ParserTest.class.getResourceAsStream(resource);
         if(in == null) {
-        	throw new Exception("failed to load resource " + resource);
+            throw new Exception("failed to load resource " + resource);
         }
         return in;
-	}
+    }
 
     public static void main(String[] args) throws Exception {
         Parser parser = new Parser(new Resolver() {
             @Override
             public InputStream resolve(String name) throws Exception {
-                return ParserTest.class.getResourceAsStream("/ereefs/boxrenderer/xhtml/"+name);
+                return ParserTest.class.getResourceAsStream(name);
             }});
         final Box burdekinBox = parser.parse(getStream(BURDEKIN_STATUS));
         final Box capeyorkBox = parser.parse(getStream(CAPE_YORK_STATUS));
@@ -50,12 +50,12 @@ public class ParserTest {
                         Graphics2D g2 = (Graphics2D)g;
                         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
                         try {
-                        	Dimension burd = burdekinBox.getDimension(g2);
-                        	Dimension cape = capeyorkBox.getDimension(g2);
-                        	burdekinBox.render(g2);
-                        	Graphics2D g3 = (Graphics2D)g2.create(burd.width+1, 0, cape.width, cape.height);
-                        	capeyorkBox.render(g3);
-                        	g3.dispose();
+                            Dimension burd = burdekinBox.getDimension(g2);
+                            Dimension cape = capeyorkBox.getDimension(g2);
+                            burdekinBox.render(g2);
+                            Graphics2D g3 = (Graphics2D)g2.create(burd.width+1, 0, cape.width, cape.height);
+                            capeyorkBox.render(g3);
+                            g3.dispose();
                         } catch (Exception e) {
                             // TODO Auto-generated catch block
                             throw new RuntimeException(e);
